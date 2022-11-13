049f397be4b74e10c89337516983e199fb6055b9
Jirat Sukphalanon <6210612633@cn330>
2022-11-13 21:23:39 +0700

    1.4 handleMap() insert new map log to DB

--- api/map.js
+++ api/map.js
@@ -8,9 +8,14 @@ exports.handleMap = (req, res) => {
     var resp = {}
 
     // Q1.3 extract userId and from(lat/lng) to(locationId) from req.body 
-    
-    // Q1.4 insert new map log into database
 
+    // Q1.4 insert new map log into database
+    MapLogModel.create({
+        userId: userId,
+        fromLoc: fromLoc,
+        toLoc: toLoc,
+        createdAt: Date.now()
+    });
     res.status(200).json(resp);
 }
     
