142a7c4695e13ca37b496b3e249fc3ea8b0040fd
Pattana Sapphanachai <60337808+6210612831@users.noreply.github.com>
2022-11-13 23:45:22 +0700

    Q3.2 Q3.3 Q3.4

--- api/info.js
+++ api/info.js
@@ -35,19 +35,36 @@ exports.populateInfo = () => {
 }
 
 // search for information related to keywords
-exports.handleInfo = (req, res) => {
-    var resp = {}
-
+exports.handleInfo = async (req, res) => {
+    var resp = {};
+    var output = {};
     // Q3.2 extract userId and keywords (as Array) from req.query 
+    if(req.query.keywords){
+        var keywords=req.query.keywords.split(',');
+    }
+    if(req.query.userId){
+        var userId=req.query.userId;
+    }
     
+    // console.log(keywords);
     if (keywords) {
         // Q3.3 find all info related to keywords (use regex in Mongoose criteria) 
-
+        for (const id in keywords) {
+            output[keywords[id]]= await InfoModel.find({infoTitle: { $regex: "(?i)"+keywords[id]+"(?-i)" }});
+        };
+        console.log(output);
         // Q3.4 aggreate multiple info as one if multiple keywords are provided
-
+        var temp = [];
+        for (const key in output) {
+            for (const index in output[key]) {
+                temp.push(output[key][index]);
+            }
+        }
+        resp = temp;
         // Q3.5 insert new info log into database
 
-    } else {
+    } 
+    else {
         // Q3.6 get name/phone/email from req.query
 
         // Q3.7 add new contact into database
