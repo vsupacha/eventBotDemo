d86a0c279075a514daed10fd5f7b813bbc07ea27
6210612682 <6210612682@student.tu.ac.th>
2022-11-13 22:04:15 +0700

    push Q1.3

--- api/map.js
+++ api/map.js
@@ -8,6 +8,10 @@ exports.handleMap = (req, res) => {
     var resp = {}
 
     // Q1.3 extract userId and from(lat/lng) to(locationId) from req.body 
+    var userId = req.query.userId;
+    var fromLoc = req.query.fromLoc;
+    var toLoc = req.query.toLoc;
+    resp = {userId, fromLoc, toLoc};
 
     // Q1.4 insert new map log into database
     MapLogModel.create({
